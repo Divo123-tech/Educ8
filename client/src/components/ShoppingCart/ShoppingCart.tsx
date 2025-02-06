aed4a5526c3b4efec8c0fcefedf7bfc062dd1707
Divo123 <topherharsh@gmail.com>
2025-02-06 14:31:33 +0800

    edited shopping cart to have skeleton

--- client/src/components/ShoppingCart/ShoppingCart.tsx
+++ client/src/components/ShoppingCart/ShoppingCart.tsx
@@ -10,6 +10,7 @@ import { UserContext } from "@/context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { getCurrentUser } from "@/services/users.service";
 import Pagination from "../Pagination";
+import { Skeleton } from "../ui/skeleton";
 const ShoppingCart = () => {
   const navigate = useNavigate();
 
@@ -25,13 +26,16 @@ const ShoppingCart = () => {
   const [nextPage, setNextPage] = useState<string | null>(null);
   const [total, setTotal] = useState<number>(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const response = await getCartItems(currentPage);
       setCartItems(response.results);
       setPreviousPage(response.previous);
       setNextPage(response.next);
       setTotal(response.count);
+      setLoading(false);
     })();
     const checkUserLoggedIn = async () => {
       try {
@@ -75,20 +79,40 @@ const ShoppingCart = () => {
           <p className="font-bold text-md">{total} Courses in Cart</p>
           <hr></hr>
           <div>
-            {cartItems?.map((cartItem: CartItem) => {
-              return (
-                <CourseCartView
-                  id={cartItem.id}
-                  key={cartItem.id}
-                  course={cartItem.course}
-                  currentPage={currentPage}
-                  setCartItems={setCartItems}
-                  setPreviousPage={setPreviousPage}
-                  setNextPage={setNextPage}
-                  setTotal={setTotal}
-                />
-              );
-            })}
+            {loading ? (
+              Array.from({ length: 5 }, (_, i) => i + 1).map(
+                (number: number) => (
+                  <div
+                    className="flex gap-4 w-full border-b p-2 py-4 rounded-xl"
+                    key={number}
+                  >
+                    <Skeleton className="w-40 h-28 sm:w-28 sm:h-24 object-cover object-center pt-2 sm:pt-0" />
+                    <div className="flex flex-col justify-between w-full">
+                      <Skeleton className="h-4 w-1/6" />
+                      <Skeleton className="h-4 w-1/3" />
+                      <Skeleton className="h-4 w-1/2" />
+                    </div>
+                  </div>
+                )
+              )
+            ) : (
+              <>
+                {cartItems?.map((cartItem: CartItem) => {
+                  return (
+                    <CourseCartView
+                      id={cartItem.id}
+                      key={cartItem.id}
+                      course={cartItem.course}
+                      currentPage={currentPage}
+                      setCartItems={setCartItems}
+                      setPreviousPage={setPreviousPage}
+                      setNextPage={setNextPage}
+                      setTotal={setTotal}
+                    />
+                  );
+                })}
+              </>
+            )}
           </div>
         </div>
         <div className="flex flex-col py-2 gap-4">
