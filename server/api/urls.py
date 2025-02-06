e095160d61eec7a8880c97a75f09a7d26b78b64e
Divo123 <topherharsh@gmail.com>
2025-02-06 18:00:26 +0800

    made login use email not username

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
-from .views import UserView, RegisterView, FetchMessagesView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, FetchMessagesView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
          FetchMessagesView.as_view(), name='fetch_messages'),
     path('analytics/', include('api.urls_folder.analytics')),
     path('register/', RegisterView.as_view(), name="register-user"),
-    path("token/", TokenObtainPairView.as_view(), name="token-request"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token-request"),
     path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
     path("auth/", include("rest_framework.urls")),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
