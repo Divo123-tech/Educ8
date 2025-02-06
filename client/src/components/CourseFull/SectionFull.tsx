b02cd3122772bcaf226974d3e07a8844ae565571
Divo123 <topherharsh@gmail.com>
2025-02-06 17:00:37 +0800

    stash

--- client/src/components/CourseFull/SectionFull.tsx
+++ client/src/components/CourseFull/SectionFull.tsx
@@ -7,16 +7,13 @@ import {
 } from "../ui/accordion";
 import { Content, getContentsDetailed } from "@/services/content.services";
 import { FileIcon, MonitorPlay, Image } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
 
 type Props = {
   courseId: string | number;
   section: Section;
   value: number;
-  selectSectionContent: (
-    contentId: Content
-    // nextContent: Content | null,
-    // prevContent: Content | null
-  ) => void;
+  selectSectionContent: (contentId: Content) => void;
 };
 const SectionFull = ({
   courseId,
@@ -25,43 +22,55 @@ const SectionFull = ({
   selectSectionContent,
 }: Props) => {
   const [contents, setContents] = useState<Content[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
-    (async () => {
-      const contents = await getContentsDetailed(courseId, section.id);
-      setContents(contents);
-    })();
-  }, [courseId, section]);
+    if (isOpen) {
+      (async () => {
+        const contents = await getContentsDetailed(courseId, section.id);
+        setContents(contents);
+        setLoading(false);
+      })();
+    }
+  }, [courseId, isOpen, section]);
 
   return (
     <AccordionItem value={`item-${value}`}>
-      <AccordionTrigger className="px-4">
+      <AccordionTrigger
+        className="px-4"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
         Section {section.position}: {section.title}
         <p className="ml-auto font-normal text-gray-700 text-xs">
           {section.contents.length} lectures
         </p>
       </AccordionTrigger>
       <AccordionContent className="bg-white flex flex-col">
-        {contents.map((content: Content) => {
-          return (
-            <div
-              key={content.id}
-              className="flex items-center gap-4 px-10 py-3 hover:bg-gray-200 cursor-pointer"
-              onClick={() =>
-                selectSectionContent(
-                  content
-                  // contents.length > index + 1 ? contents[index + 1] : null,
-                  // index != 0 ? contents[index - 1] : null
-                )
-              }
-            >
-              {content.contentType == "text" && <FileIcon size={18} />}
-              {content.contentType == "video" && <MonitorPlay size={18} />}
-              {content.contentType == "image" && <Image size={18} />}
+        {loading ? (
+          <div className="flex flex-col gap-4 px-10 py-3 hover:bg-gray-200 cursor-pointer">
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-full h-4" />
+            <Skeleton className="w-full h-4" />
+          </div>
+        ) : (
+          <>
+            {contents.map((content: Content) => {
+              return (
+                <div
+                  key={content.id}
+                  className="flex items-center gap-4 px-10 py-3 hover:bg-gray-200 cursor-pointer"
+                  onClick={() => selectSectionContent(content)}
+                >
+                  {content.contentType == "text" && <FileIcon size={18} />}
+                  {content.contentType == "video" && <MonitorPlay size={18} />}
+                  {content.contentType == "image" && <Image size={18} />}
 
-              <p className="">{content.title}</p>
-            </div>
-          );
-        })}
+                  <p className="">{content.title}</p>
+                </div>
+              );
+            })}
+          </>
+        )}
       </AccordionContent>
     </AccordionItem>
   );
