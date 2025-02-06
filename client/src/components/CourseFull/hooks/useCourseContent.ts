83102f126eb55ef1d66cce6104e38babebfa4602
Divo123 <topherharsh@gmail.com>
2025-02-06 17:05:25 +0800

    optimized the Course full page

---
+++ client/src/components/CourseFull/hooks/useCourseContent.ts
@@ -0,0 +1,62 @@
+import { User } from "@/context/UserContext";
+import { Content, getSingleContent } from "@/services/content.services";
+import { Course, getSingleCourse } from "@/services/courses.service";
+import { getSections, Section } from "@/services/sections.services";
+import { getUserInfo } from "@/services/users.service";
+import { useEffect, useState } from "react";
+
+// Custom hook for course content
+const useCourseContent = (courseId: string) => {
+  const [course, setCourse] = useState<Course | null>(null);
+  const [sections, setSections] = useState<Section[] | null>(null);
+  const [currentContent, setCurrentContent] = useState<Content | null>(null);
+  const [instructor, setInstructor] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchCourseContent = async () => {
+      try {
+        const [courseData, sectionsData] = await Promise.all([
+          getSingleCourse(courseId),
+          getSections(courseId),
+        ]);
+
+        setCourse(courseData);
+        setSections(sectionsData);
+
+        if (sectionsData?.length > 0) {
+          const firstContentId = sectionsData[0].contents[0];
+          const content = await getSingleContent(
+            courseId,
+            sectionsData[0].id,
+            firstContentId
+          );
+          setCurrentContent(content);
+        }
+
+        if (courseData?.creator.id) {
+          const instructorData = await getUserInfo(courseData.creator.id);
+          setInstructor(instructorData);
+        }
+      } catch (error) {
+        console.error("Error fetching course content:", error);
+      }
+    };
+
+    if (courseId) {
+      fetchCourseContent();
+    }
+  }, [courseId]);
+
+  const selectSectionContent = async (content: Content) => {
+    setCurrentContent(content);
+  };
+
+  return {
+    course,
+    sections,
+    currentContent,
+    instructor,
+    selectSectionContent,
+  };
+};
+export default useCourseContent;
